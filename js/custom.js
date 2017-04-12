8497237dff5f8e976e26a3256ecbf6a1fbb0c073
Oksana Bondar <opokhmurska@gmail.com>
2017-04-12 11:21:47 +0000

    add iframe

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+(function($){
+
+	/* ---------------------------------------------- /*
+	 * Preloader
+	/* ---------------------------------------------- */
+
+	$(window).load(function() {	
+		
+		$('#status').fadeOut();
+		$('#preloader').delay(500).fadeOut('slow');
+		
+	});
+
+	$(document).ready(function() {
+
+		/* ---------------------------------------------- /*
+		 * Smooth scroll / Scroll To Top
+		/* ---------------------------------------------- */
+
+		$('a[href*=#]').bind("click", function(e){
+           
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('.scroll-up').fadeIn();
+			} else {
+				$('.scroll-up').fadeOut();
+			}
+		});
+
+
+	});
+	
+
+})(jQuery);
