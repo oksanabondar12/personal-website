cd8a050da027ae9f55494916a80cf84a584ddfb6
Oksana Bondar <opokhmurska@gmail.com>
2017-04-13 19:38:53 +0000

    fix html and js files

--- js/form.js
+++ js/form.js
@@ -3,11 +3,8 @@ $(function() {
 });
 
 function sendForm(e) {
-    var form = document.querySelector('form');
-    if (!form[0].checkValidity()) {
-        form.find(':submit').click(); // We try to submit invalid form to show message 
-    }
-    else {
+  var form = document.querySelector('form');
+  if (form.checkValidity()) {
         e.preventDefault();
         $.ajax({
                 url: "https://formspree.io/opokhmurska@gmail.com",
